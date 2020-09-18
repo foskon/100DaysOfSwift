ab31cd442da4256533d8da6661a45b8356797f28
Carlos Manzanas <foskon@gmail.com>
2020-09-18 10:41:32 +0200

    (Day 16) Project one - Part one

---
+++ HWSP1/HWSP1/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    private var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { pictures.count }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
