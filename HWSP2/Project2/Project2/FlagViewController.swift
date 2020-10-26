8418edba63305e50e5642d75a7a50322d22bde47
Carlos Manzanas <foskon@gmail.com>
2020-10-26 16:21:46 +0100

    (Days 20 - 21) Project 2, part three - Add logic to flags app. #100DaysOfSwift

--- HWSP2/Project2/Project2/FlagViewController.swift
+++ HWSP2/Project2/Project2/FlagViewController.swift
@@ -7,6 +7,7 @@ class FlagViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     private var countries: [String]!
+    private var correctAnswer = 0
     private var score: Int!
     
     class func instantiate() -> FlagViewController {
@@ -27,9 +28,30 @@ class FlagViewController: UIViewController {
     }
     
     func askQuestion() {
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = "Score: \(score ?? 0) - " + countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func onTouchFlag(_ sender: UIButton) {
+        let isRightAnswer = toFlagNumber(sender) == correctAnswer
+        let title = isRightAnswer ? "Correct" : "Wrong"
+        score = isRightAnswer ? score + 1 : score - 1
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score ?? 0).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in
+            self.askQuestion()
+        }))
+        present(ac, animated: true)
+    }
+    
+    private func toFlagNumber(_ sender: UIButton) -> Int {
+        if sender == button1 { return 0 }
+        else if sender == button2 { return 1 }
+        else { return 2 }
     }
 }
 
